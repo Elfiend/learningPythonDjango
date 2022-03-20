8c859d8b0340b216977c90162e7adac790e8d622
Elfiend <elfiend@gmail.com>
2022-03-20 23:03:50 +0800

    Error: AUTH_USER_MODEL refers to model 'login.local_accounts' that has not been installed.

--- login/admin.py
+++ login/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from .local.accounts import LocalUser
+
+admin.site.register(LocalUser, UserAdmin)
+
+# unregister the Group model from admin.
+admin.site.unregister(Group)
