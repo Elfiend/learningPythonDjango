f8b0471d83d841d006f86ce85ebe98cdb69f2562
Elfiend <elfiend@gmail.com>
2022-03-22 02:59:07 +0800

    Adding reset password validators.
    Finish the following requirement.
    ```
    Reset Password [30 points]
    ```
    
    Reference:
    google 'django post form'
    https://docs.djangoproject.com/en/4.0/topics/forms/
    google 'python django form password validation'
    https://stackoverflow.com/questions/11679918/django-form-for-resetting-the-password
    Get the keywords 'PasswordChangeForm' and google 'python django PasswordChangeForm'
    https://www.programcreek.com/python/example/50650/django.contrib.auth.forms.PasswordChangeForm
    
    google 'python django PasswordChangeForm custom validation'
    https://sixfeetup.com/blog/custom-password-validators-in-django
    
    Error Fixing.
    ImportError: cannot import name 'ugettext' from 'django.utils.translation'
    https://stackoverflow.com/questions/71420362/django4-0-importerror-cannot-import-name-ugettext-lazy-from-django-utils-tra

--- pyexam/settings.py
+++ pyexam/settings.py
@@ -90,18 +90,14 @@ DATABASES = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 8, }
+     },
+    {'NAME': 'login.validators.NumberValidator', },
+    {'NAME': 'login.validators.UppercaseValidator', },
+    {'NAME': 'login.validators.LowercaseValidator', },
+    {'NAME': 'login.validators.SymbolValidator', },
 ]
 
 
