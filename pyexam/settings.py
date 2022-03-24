1645e29c8f0c6e9ca457cefdfa2fcef9d477aa32
Elfiend <elfiend@gmail.com>
2022-03-25 02:15:55 +0800

    Used the heroku for CI/CD.
    Reference:
    https://devcenter.heroku.com/articles/build-docker-images-heroku-yml

--- pyexam/settings.py
+++ pyexam/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'elfiend.ddns.net',
+    'qa-elfiend.herokuapp.com',
+]
 
 
 # Application definition
